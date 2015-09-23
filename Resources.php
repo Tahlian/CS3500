4ad9a7877c101cd3b85f65a0994435ed8c74c337
Andrew Gifford <Andrew.Gifford@smkazoo.com>
2015-09-23 15:19:09 -0400

    Added Resources page

---
+++ Resources.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Resources</title>
+</head>
+<body>
+	<?php include 'menu.html'; ?>
+	<h3>Resources</h3>
+	
+	<p>This page lists various resources for HTML and CSS.  The HTML and CSS standards can be found at <a href="http://www.w3.org/standards/">W3C</a>.</p>
+	
+	<h4>Tutorials</h2>
+	<ul>
+		<li><a href="http://www.html-5-tutorial.com/">Basic HTML5 Tutorial</a></li>
+		<li><a href="http://www.w3schools.com/html/">various HTML Tutorials</a></li>
+		<li><a href="http://www.w3schools.com/css/">Various CSS Tutorials</a></li>
+		<li><a href="http://www.html5rocks.com/en/tutorials/getusermedia/intro/">HTML5 Webcam tutorial</a></li>
+	</ul>
+	<h4>Tools</h4>
+	<ul>
+		<li><a href="https://html5test.com/">HTML5 browser compatibility test</a></li>
+		<li><a href="https://validator.w3.org">HTML Validator</a></li>
+		<li><a href="http://jigsaw.w3.org/css-validator/">CSS Validator</a></li>
+		<li><a href="https://dev.modern.ie/tools/staticscan/">IE Compatibility Scan</a></li>
+		<li><a href="https://developers.google.com/speed/pagespeed/insights/">Page Speed Test</a></li>
+		<li><a href="https://www.google.com/webmasters/tools/mobile-friendly/">Mobile Friendly Test</a></li>
+	</ul>
+</body>
+</html>
