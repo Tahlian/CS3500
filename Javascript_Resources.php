6b0124112085927e153614b19e380573e5705946
Jonathan Pennington <jonathan.s.pennington@wmich.edu>
2015-10-05 15:46:04 -0400

    Uploaded a base for the Javascript Resources page

---
+++ Javascript_Resources.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Javascript Resources</title>
+</head>
+<body>
+	<?php include 'menu.html'; ?>
+	<div class="content">
+	<h3 class="title">Javascript Resources</h3>
+	
+	<p>This page lists various resources for Javascript. The Javascript standards can be found at <a href="http://www.ecma-international.org/ecma-262/6.0/index.html">ECMA-International</a>.</p>
+	
+	<h4>Tutorials</h4>
+	<ul>
+		<li><a href="https://www.thenewboston.com/videos.php?cat=10">Javascript Tutorial Videos</a></li>
+	</ul>
+	<h4>Tools</h4>
+	<ul>
+		<li><a href="https://addons.mozilla.org/en-US/firefox/addon/firebug/">Firebug (Javascript Debugging Add-On for Firefox)</a></li>
+	</ul>
+	<h4>Specifications</h4>
+	<ul>
+		<li><a href="http://www.ecma-international.org/ecma-262/6.0/index.html">Javascript Specifications</a></li>
+	</ul>
+	</div>
+</body>
+</html>
