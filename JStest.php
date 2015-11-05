494dff625382a388d878f7d7dc17ace54e23aaaf
Jordan Hinds <ISurvivedRavenholm@gmail.com>
2015-11-04 22:21:24 -0500

    Added adapter.js to js. Changed JStest.php and main.js so that camera selecting can now be done in Firefox and Chrome.

--- JStest.php
+++ JStest.php
@@ -2,66 +2,18 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="mystyle.css">
-	<script src="../js/lib/ga.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Resources</title>
 	<base target="_blank">
-	<style>
-	#webcam,
-    #feed {
-        display: none;
-    }
-	video {
-	  object-fit: cover;
-	}
-	
-	@media screen and (min-width: 1000px) {
-	  video {
-	    height: 480px;
-	  }
-	}
-	</style>
 </head>
 <body>
-	<div class="everything">
-	<?php include 'menu.html'; ?>
-	<div class="content">
-	<h3>Test</h3><br>
-
-	<p>This page is a Test environment for Javascript/JQuery programs.</p>
-	<div class="select">
-		<label for="videoSource">Video source: </label><select id="videoSource"></select>
-	</div>
-	<video id="taggable" autoplay=""></video>
-	<canvas id="feed"></canvas>
-	<canvas id="display"></canvas>
-	<script type="text/javascript">
-	window.requestAnimationFrame ||
-        (window.requestAnimationFrame = window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function( callback ){
-                window.setTimeout(callback, 1000 / 60);
-            });
-	var webrtc = (function() {
-        var feed = document.getElementById('feed'),
-            feedContext = feed.getContext('2d'),
-            display = document.getElementById('display'),
-            displayContext = display.getContext('2d');
-
-        // our existing code.
-
-        function streamFeed() {
-            requestAnimationFrame(streamFeed);
-            displayContext.drawImage(video, 0, 0, display.width, display.height);
-        }
-    })();
-	</script>
-	<script src="js/main.js"></script>
-	</div>
-	</div>
+<?php include 'menu.html'; ?>
+<video id="video" width="800" height="600" style="max-width:100%;background:#111;border:1px solid #666;" autoplay></video>
+<button id="MediaStreamStartButton">Start</button> <button id="MediaStreamStopButton">Stop</button>
+<select id="videoSource"></select>
+<script src=js/adapter.js></script>
+<script src=js/main.js></script>
 </body>
 </html>
 
--- js/main.js
+++ js/main.js
@@ -1,124 +1,19 @@
 'use strict';
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+var videoElement = document.querySelector('video');
+var videoSelect = document.getElementById("videoSource");
 
-var constraints = {audio: false, video: true};
-var video = document.querySelector('video');
+navigator.getUserMedia = navigator.getUserMedia ||
+  navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-function successCallback(stream) {
-  window.stream = stream; // stream available to console
-  if (window.URL) {
-    video.src = window.URL.createObjectURL(stream);
-  } else {
-    video.src = stream;
-  }
- // source.canvas.width = video.videoWidth;
- // source.canvas.height = video.videoHeight;
- // draw();
-}
-
-function errorCallback(error){
-  console.log('navigator.getUserMedia error: ', error);
-}
-
-//navigator.getUserMedia(constraints, successCallback, errorCallback);
-
-
-
-function clickTag(){
-   $("#taggable").click(function(e){
-    	//var x = e.pageX - this.offsetLeft;
-    	//var y = e.pageY - this.offsetTop;
-	var x = e.pageX - $(this).offset().left;
-	var y = e.pageY - $(this).offset().top;
-    	//var url = $(this).attr("href");
-    	//courl = url + '&x=' + x + '&y=' + y;
-	
-/*      $.ajax({
-    	type:"GET",
-     	url: courl,
-    	dataType:"script"
-    	});*/
-	console.log(x);
-	console.log(y);
-	
-	var imgData = source.getImageData(x,y,10,10);
-	var Aves = getAverage(imgData);
-	alert("red=" + Aves[0] + "  green=" + Aves[1] + "  blue=" + Aves[2]);
-    	return false;
-   }); 
-}
-
-function getAverage(imgData){
-	var redTotal = 0;
-	var greenTotal = 0;
-	var blueTotal = 0;
-	var total = 0;
-	for (var i=0;i<imgData.data.length;i+=4)
-	{
-		redTotal += imgData.data[i+0];
-		greenTotal += imgData.data[i+1];
-		blueTotal += imgData.data[i+2];
-		total++;
-	}
-	var Averages = new Array;
-	Averages[0] = redTotal/total;
-	Averages[1] = greenTotal/total;
-	Averages[2] = blueTotal/total;
-	
-	return Averages;
-}
-
-$(document).ready(function(){
-clickTag();
-});
-
-var video = document.querySelector('video');
-var source = document.getElementById('display').getContext('2d'),
-    output = source; //document.getElementById('output').getContext('2d'),
-    //target = document.getElementById('display');
-var videoSelect = document.querySelector('select#videoSource');
-
-video.addEventListener('loadedmetadata', function () {
-  // due to bug in Chrome: http://crbug.com/168700
-  if (video.videoWidth) {
-    source.canvas.width = video.videoWidth;
-    source.canvas.height = video.videoHeight;
-  }
-  draw();
-});
-
-
-function draw() {
-  requestAnimFrame(draw);
-  source.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, source.canvas.width, source.canvas.height);
-  var pixels = source.getImageData(0, 0, source.canvas.width, source.canvas.height),
-      i = 0,
-      brightness;
-  output.putImageData(pixels, 0, 0);
-}
-
-
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       || 
-          window.webkitRequestAnimationFrame || 
-          window.mozRequestAnimationFrame    || 
-          window.oRequestAnimationFrame      || 
-          window.msRequestAnimationFrame     || 
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-
-
-function gotSources(sourceInfos) {
+/*function gotSources(sourceInfos) {
   for (var i = 0; i !== sourceInfos.length; ++i) {
     var sourceInfo = sourceInfos[i];
     var option = document.createElement('option');
     option.value = sourceInfo.id;
     if (sourceInfo.kind === 'audio') {
-      //option.text = sourceInfo.label || 'microphone ' + (audioSelect.length + 1);
-     // audioSelect.appendChild(option);
+      option.text = sourceInfo.label || 'microphone ' + (audioSelect.length + 1);
+      audioSelect.appendChild(option);
     } else if (sourceInfo.kind === 'video') {
       option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
       videoSelect.appendChild(option);
@@ -126,34 +21,56 @@ function gotSources(sourceInfos) {
       console.log('Some other kind of source: ', sourceInfo);
     }
   }
+}*/
+
+if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+	  alert("enumerateDevices() not supported.");
+	} else {
+		navigator.mediaDevices.enumerateDevices()
+		.then(function(devices) {
+			  devices.forEach(function(device) {
+			    console.log(device.kind + ": " + device.label +
+			                " id = " + device.deviceId);
+			    var option = document.createElement("option");
+			    option.value = device.deviceId;
+			    if (device.kind == 'videoinput') {
+			        option.text = device.label || 'camera ';
+			        videoSelect.add(option);
+			    } else {
+			    	console.log('Some other kind of source: ', device);
+			    }
+			  });
+			});
+		}
+
+
+function successCallback(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.src = window.URL.createObjectURL(stream);
+  videoElement.play();
 }
 
-if (typeof MediaStreamTrack === 'undefined'){
-  alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
-} else {
-  MediaStreamTrack.getSources(gotSources);
+function errorCallback(error){
+  console.log('navigator.getUserMedia error: ', error);
 }
 
 function start(){
   if (!!window.stream) {
-    video.src = null;
+    videoElement.src = null;
     window.stream.stop();
   }
-  //var audioSource = audioSelect.value;
   var videoSource = videoSelect.value;
   var constraints = {
-    /*audio: {
-      optional: [{sourceId: audioSource}]
-    },*/
     video: {
       optional: [{sourceId: videoSource}]
-    }
+    },
+    audio: false
   };
   navigator.getUserMedia(constraints, successCallback, errorCallback);
 }
 
+document.getElementById('MediaStreamStartButton').onclick = start;
 videoSelect.onchange = start;
-
-start();
+document.getElementById('MediaStreamStopButton').onclick = function() {if (window.stream) { window.stream.stop(); }};
 
 
